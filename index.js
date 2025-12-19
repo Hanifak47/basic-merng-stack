47ea1190b4de2be08c4e36c595c72fa0e3657b50
Hanifak47 <hanifsmurf@gmail.com>
2025-12-19 09:47:07 +0700

    + fetching mechanism work

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post');
+// Memanggil variabel MONGODB dari file config.js
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
-// setiap query mutation atau subscribtiion akan ada resolvernya sendiri
 const resolvers = {
-  Query: {
-    sayHi: () => 'Hello World!'
-  }
+    Query: {
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
+    }
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+    typeDefs,
+    resolvers
 });
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`);
-});
+// Menghubungkan ke MongoDB tanpa opsi yang sudah tidak didukung (deprecated)
+mongoose
+    .connect(MONGODB)
+    .then(() => {
+        console.log('MongoDB Connected successfully');
+        return server.listen({ port: 5000 });
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
+    .catch((err) => {
+        console.error('Connection error:', err.message);
+    });
