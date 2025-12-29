aef9786c5be1af353bf646a48dfb33527c8a5f19
Hanifak47 <hanifsmurf@gmail.com>
2025-12-29 13:00:34 +0700

    - subscribtion need be fixed
    + like toggle post mechanism

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -37,6 +37,10 @@ module.exports = {
             });
             const post = await newPost.save();
 
+            context.pubsub.publish('NEW_POST', {
+                newPost: post
+            });
+
             return post;
         },
 
@@ -55,6 +59,36 @@ module.exports = {
             } catch (err) {
                 throw new Error(err);
             }
+        },
+
+        async likePost(_, { postId }, context) {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if (post) {
+                if (post.likes.find((like) => like.username === username)) {
+                    // Post sudah di-like, maka batalkan (unlike)
+                    post.likes = post.likes.filter((like) => like.username !== username);
+                } else {
+                    // Belum di-like, tambahkan like ke post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
+
+                await post.save();
+                return post;
+            } else {
+                throw new UserInputError('Post not found');
+            }
+        }
+
+    },
+
+    Subscription: {
+        newPost: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
         }
     }
 }
