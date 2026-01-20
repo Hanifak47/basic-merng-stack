928894456ef690d321d76bafe2ee55e889e95a9c
Hanifak47 <hanifsmurf@gmail.com>
2026-01-20 23:13:37 +0700

    + bisa melakukan registrasi user

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
 
-function Register() {
+function Register(props) {
+
+    // (1) menampung error
+    const [errors, setErrors] = useState({});
+
+    // (2) menampung data ketikan user
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    // (3) setiap kali value dari form berubah maka value akan diperbaruhi sesuai inputan user secara realtime, setiap ada perubahan data values (2) akan diperbaruhi
+    const onChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    }
+
+    // (4) mendefinisikan sebuah method bernama adduser dimana variabel yg ditampung adalah data ketikan user, serta pendefinisian error, data values adalah diambil dari (2) jika submit
+    const [addUser, { loading }] = useMutation(REGISTER_USER, {
+        update(_, result) {
+            console.log(result)
+            props.history.push('/');
+        },
+        onError(err) {
+            console.log(err.graphQLErrors[0].extensions.exception.errors);
+            setErrors(err.graphQLErrors[0].extensions.exception.errors);
+        },
+        variables: values
+    });
+
+    // (5) jika submit jalankan adduser
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addUser();
+    }
     return (
-        <div>
-            <h1>Register Page</h1>
+        <div className="form-container">
+            <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ''}>
+                <h1>Register</h1>
+                <Form.Input
+                    label="Username"
+                    placeholder="Username.."
+                    name="username"
+                    type="text"
+                    value={values.username}
+                    error={errors.username ? true : false}
+                    onChange={onChange}
+                />
+
+                <Form.Input
+                    label="Email"
+                    placeholder="Email.."
+                    name="email"
+                    type="email"
+                    value={values.email}
+                    error={errors.email ? true : false}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label="Password"
+                    placeholder="Password.."
+                    name="password"
+                    type="password"
+                    value={values.password}
+                    error={errors.password ? true : false}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label="Confirm Password"
+                    placeholder="Confirm Password.."
+                    name="confirmPassword"
+                    type="password"
+                    value={values.confirmPassword}
+                    error={errors.confirmPassword ? true : false}
+                    onChange={onChange}
+                />
+                <Button type="submit" primary onSubmit={onSubmit}>
+                    Register
+                </Button>
+
+            </Form>
+
+
+            {/* list errornya */}
+
+            {Object.keys(errors).length > 0 && (
+                <div className="ui error message">
+                    <ul className="list">
+                        {Object.values(errors).map((value) => (
+                            <li key={value}>{value}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+
         </div>
     );
 }
+
+const REGISTER_USER = gql`
+# lihat pada resolvers user
+  mutation register(
+    $username: String!
+    $email: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      registerInput: {
+        username: $username
+        email: $email
+        password: $password
+        confirmPassword: $confirmPassword
+      }
+    ) {
+#    returnnya
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
 
 export default Register;
